a93c5fc92566992da646b0a251afbd0109af21dd
young924 <ahenaka9@gmail.com>
2021-01-08 23:53:19 +0900

    Solve 16987

---
+++ backjoon/backtracking/16987.py
@@ -0,0 +1,45 @@
+#
+# 16987. 계란으로 계란치기
+# https://www.acmicpc.net/problem/15649
+#
+
+import sys
+
+eggs = []
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    eggs.append(list(map(int, sys.stdin.readline().split())))
+
+def hit(i, cnt):
+    if i == N:
+        return cnt
+
+    if eggs[i][0] <= 0:
+        return hit(i+1, cnt)
+
+    maxCnt = cnt
+
+    for j in range(N):
+        if i == j: continue
+        if eggs[j][0] <= 0: continue
+
+        eggs[i][0] -= eggs[j][1]
+        eggs[j][0] -= eggs[i][1]
+        # print('hit', i, j)
+
+        if eggs[i][0] <= 0 and eggs[j][0] <= 0:
+            # print('calling hit', i+1, 'cnt', cnt+2)
+            maxCnt = max(hit(i+1, cnt+2), maxCnt)
+        elif eggs[i][0] <= 0 or eggs[j][0] <= 0:
+            # print('calling hit', i+1, 'cnt', cnt+1)
+            maxCnt = max(hit(i+1, cnt+1), maxCnt)
+        else:
+            maxCnt = max(hit(i+1, cnt), maxCnt)
+
+        eggs[i][0] += eggs[j][1]
+        eggs[j][0] += eggs[i][1]
+    
+    return maxCnt
+
+print(hit(0, 0))
