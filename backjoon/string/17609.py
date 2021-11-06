6ce4bcd7a7f37a3b0e1af8ec715e794dc35244e9
young924 <ahenaka9@gmail.com>
2021-11-07 00:36:12 +0900

    Fix 17609

--- backjoon/string/17609.py
+++ backjoon/string/17609.py
@@ -3,44 +3,39 @@
 # https://www.acmicpc.net/problem/17609
 #
 
-############# Time out #############
-
 import sys
 
-def psuedo_palindrome(str):
-
-    for i in range(len(str)):   # exclude ith char
-        left = 0
-        right = len(str) - 1
-
-        is_palindrome = True
-        
-        while (left < right):
-            if str[left] != str[right]:
-                is_palindrome = False
-                break
-
-            left += 1
-            right -= 1
-
-            if left == i: left += 1
-            elif right == i: right -= 1
-        
-        if is_palindrome: return True
-
-    return False
-
-def palindrome(str):
+def psuedo_palindrome(left, right, str):
     is_palindrome = True
-    half_len = len(str) // 2
 
-    for i in range(half_len):
-        if str[i] != str[-i-1]:
+    while left < right:
+        if str[left] != str[right]:
             is_palindrome = False
             break
+
+        left += 1
+        right -= 1
+    
+    return is_palindrome
+
+def palindrome(left, right, str):
+    is_palindrome = True
+
+    while left < right:
+        if str[left] != str[right]:
+            is_palindrome = False
+            
+            if psuedo_palindrome(left+1, right, str):
+                return 1
+            if psuedo_palindrome(left, right-1, str):
+                return 1
+            
+            break
+
+        left += 1
+        right -= 1
     
     if is_palindrome: return 0
-    elif psuedo_palindrome(str): return 1
     else: return 2
 
 T = int(sys.stdin.readline())
@@ -48,6 +43,6 @@ result = ""
 
 for i in range(T):
     input = sys.stdin.readline().rstrip()
-    result += f"{palindrome(input)}\n"
+    result += f"{palindrome(0, len(input) - 1, input)}\n"
 
 print(result)
